c396c3ee44ad25173f4fea89b2e71a1289818cfa
Kamy41 <30781801+Kamy41@users.noreply.github.com>
2025-01-03 00:29:02 +0100

    Update evos_moves.asm

--- data/evos_moves.asm
+++ data/evos_moves.asm
@@ -326,10 +326,10 @@ ExeggcuteEvosMoves:
 ; Learnset
 	db 25, CONFUSION
 	db 28, LEECH_SEED
-	db 32, STUN_SPORE
+	db 33, STUN_SPORE
 	db 37, POISONPOWDER
 	db 42, SOLARBEAM
-	db 47, SLEEP_POWDER
+	db 48, SLEEP_POWDER
 	db 0
 
 GrimerEvosMoves:
